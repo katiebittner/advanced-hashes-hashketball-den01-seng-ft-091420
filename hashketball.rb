c8e15d808c957cb8cc1d0ba1a16da2637ff26d64
Katie B <katiebittner1@gmail.com>
2020-09-15 15:54:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,9 +128,14 @@ end
 
 def num_points_scored(player_name)
 
-  player = player_stats(player_name)
-  player[:points]
-
+  #player = player_stats(player_name)
+  player = game_hash.each do |side, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_name
+        return player[:points]
+      end
+    end
+  end
 end
 
 def shoe_size(player_name)
